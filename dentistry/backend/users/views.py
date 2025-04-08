8a971121c2c10ad4e2945f8495f9c493b72273f1
Michael Dudkin <mishadudkin7@gmail.com>
2025-04-08 23:52:53 +0300

    Add schems for validation

--- dentistry/backend/users/views.py
+++ dentistry/backend/users/views.py
@@ -8,6 +8,7 @@ from rest_framework import permissions
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError
 
 from .paginations import PageLimitPagination
 
@@ -59,6 +60,38 @@ class CustomUserViewSet(UserViewSet):
             return Response({"errors": "У вас нет аватара"}, status=400)
 
     def perform_update(self, serializer):
-        if not self.request.user.is_support and 'role_id' in serializer.validated_data:
+        user = self.request.user
+        data = serializer.validated_data
+
+        if not self.request.user.is_support and 'role_id' in data:
             return Response({"errors": "У вас нет прав"}, status=403)
+
+        role_id = data.get('role_id', user.role_id)
+        role_class = user.ROLES.get(role_id)
+
+        if not role_class:
+            raise ValidationError({"role_id": "Неизвестная роль"})
+
+        schema = role_class.schema
+        allowed_fields = schema.get("fields", set())
+        required_fields = schema.get("required_fields", set())
+
+        # Получаем additional_info (новое или текущее)
+        additional_info = data.get('additional_info', user.additional_info or {})
+
+        if additional_info:
+            info_keys = set(additional_info.keys())
+
+            extra_fields = info_keys - allowed_fields
+            missing_fields = required_fields - info_keys
+
+            if extra_fields:
+                raise ValidationError({
+                    "additional_info": f"Недопустимые поля: {', '.join(extra_fields)}"
+                })
+            if missing_fields:
+                raise ValidationError({
+                    "additional_info": f"Обязательные поля отсутствуют: {', '.join(missing_fields)}"
+                })
+
         super().perform_update(serializer)
