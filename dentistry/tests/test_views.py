bfe9ed2f438a780aa7c5da76650e4e4d334313ac
dariaomnom <dariaomnom@gmail.com>
2025-04-13 16:37:07 +0300

    client tests added

--- dentistry/tests/test_views.py
+++ dentistry/tests/test_views.py
@@ -3,6 +3,7 @@ from rest_framework.test import APIRequestFactory
 from rest_framework.response import Response
 from backend.users.models import CustomUser
 from backend.dentists.views import DentistViewSet
+from backend.clients.views import ClientViewSet
 
 @pytest.fixture
 def api_request_factory():
@@ -17,7 +18,8 @@ def dentist_user(db):
         role_id=3
     )
 
-def test_me_action_get(db, dentist_user, api_request_factory):
+
+def test_me_action_get_dentist(db, dentist_user, api_request_factory):
     view = DentistViewSet()
     request = api_request_factory.get('/dentists/me/')
     request.user = dentist_user
@@ -30,3 +32,29 @@ def test_me_action_get(db, dentist_user, api_request_factory):
     assert isinstance(response, Response)
     assert response.status_code == 200
     assert response.data["email"] == "dentist@test.com"
+
+
+@pytest.fixture
+def client_user(db):
+    return CustomUser.objects.create(
+        email="client@test.com",
+        password="testpass123",
+        role_id=2,
+        additional_info={"allergy": "лидокаин"}
+    )
+
+
+def test_me_action_get_client(db, client_user, api_request_factory):
+    view = ClientViewSet()
+    request = api_request_factory.get('/clients/me/')
+    request.user = client_user
+    view.request = request
+
+    # Мокаем метод me
+    view.me = lambda r: Response({'email': r.user.email, 'additional_info': r.user.additional_info})
+
+    response = view.me(request)
+    assert isinstance(response, Response)
+    assert response.status_code == 200
+    assert response.data["email"] == "client@test.com"
+    assert response.data["additional_info"]["allergy"] == "лидокаин"
